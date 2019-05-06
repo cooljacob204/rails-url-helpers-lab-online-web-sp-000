8139a4c84f7f97963329e06ef48f2e698a3264ad
Jacob Feldman <jacobgfeld@gmail.com>
2019-05-06 22:56:07 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,7 @@ class StudentsController < ApplicationController
   
   def activate
     @student.update(active: !@student.active)
-    204
+    redirect student_show(@student)
   end
 
   private
